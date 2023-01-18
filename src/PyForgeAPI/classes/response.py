bc6cf13ccc9cb9c9cecd6e05c1b5f79475874a77
Luis Vinicius <luisvpersy@gmail.com>
2023-01-18 16:57:17 -0300

    chore: status code variable not acess

--- src/PyForgeAPI/classes/response.py
+++ src/PyForgeAPI/classes/response.py
@@ -5,8 +5,8 @@ import json
 class Response:
   def __init__(self, request: BaseHTTPRequestHandler):
     self._request = request
-    self.status_code = None
-    self.response = None
+    self._status_code = None
+    self._response = None
     self._request.response_sent = False
 
   def send_file(self, path: str, content_type: str):
@@ -23,21 +23,21 @@ class Response:
       self._request.end_headers()
 
   def status(self, code: int) -> "Response":
-    self.status_code = code
+    self._status_code = code
     return self
   
   def json(self, response: dict) -> "Response":
-    self.response = json.dumps(response)
+    self._response = json.dumps(response)
     self.content_type = 'application/json'
     return self
   
   def text(self, response: str) -> "Response":
-    self.response = response
+    self._response = response
     self.content_type = 'text/plain'
     return self
   
   def html(self, response: str) -> "Response":
-    self.response = response
+    self._response = response
     self.content_type = 'text/html'
     return self
   
@@ -47,13 +47,13 @@ class Response:
     self._request.response_sent = True
 
   def send(self):
-    if self.status_code is None:
+    if self._status_code is None:
       raise ValueError('status code is not set')
-    if self.response is None:
+    if self._response is None:
       raise ('response is not set')
 
-    self._request.send_response(self.status_code)
+    self._request.send_response(self._status_code)
     self._request.send_header('Content-type', self.content_type)
     self._request.end_headers()
-    self._request.wfile.write(self.response.encode())
+    self._request.wfile.write(self._response.encode())
     self._request.response_sent = True
