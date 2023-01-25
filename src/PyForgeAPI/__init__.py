94329a02ccc603e0917ce1dd68cd0585d7d7b985
Gabriel Gomes da Silva <72311029+Bielgomes@users.noreply.github.com>
2023-01-25 01:02:11 -0300

    V1.3.5

--- src/PyForgeAPI/__init__.py
+++ src/PyForgeAPI/__init__.py
@@ -1,3 +1,4 @@
 from PyForgeAPI.decorators.routes import Routes
+from PyForgeAPI.decorators.module import Module
 from PyForgeAPI.classes.request import Request
 from PyForgeAPI.classes.response import Response
